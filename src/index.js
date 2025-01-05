821010a307b60789e44e794627989062328459b2
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-05 16:07:54 +0800

    Import files to index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./utils.css";
+import "./styles.css";
+import { setFaviconBasedOnTheme } from "./iconHandler";
+
+document.addEventListener("DOMContentLoaded", () => {
+  setFaviconBasedOnTheme();
+});
