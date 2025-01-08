e520372eff7fa3e40abc22466d69ab3f1c6d5538
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-08 22:22:57 +0800

    Reset index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
-import "./utils.css";
-import "./styles.css";
-import loadInbox from "./inbox";
+import "./assets/styles/utils.css";
+import "./assets/styles/styles.css";
 
-const routes = {
-  inbox: loadInbox,
-};
-
-const content = document.getElementById("content");
-
-document.addEventListener("click", (e) => {
-  if (e.target.tagName !== "BUTTON") return;
-
-  const route = routes[e.target.id];
-  if (route) {
-    content.innerHTML = "";
-    route();
-  }
-});
-
-loadInbox();
+document.addEventListener("DOMContentLoaded");
