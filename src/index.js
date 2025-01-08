44b025cad7e656388be26e9ab3fbc0b24fb34dea
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-08 21:08:09 +0800

    Add page routing using buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import "./utils.css";
 import "./styles.css";
+import loadInbox from "./inbox";
 
-document.addEventListener("DOMContentLoaded", () => {});
+const routes = {
+  inbox: loadInbox,
+};
+
+const content = document.getElementById("content");
+
+document.addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON") return;
+
+  const route = routes[e.target.id];
+  if (route) {
+    content.innerHTML = "";
+    route();
+  }
+});
+
+loadInbox();
