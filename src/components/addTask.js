21179ceab87c14dd8fafa1164084756832bbce6f
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-09 12:09:40 +0800

    Create addTask component button

---
+++ src/components/addTask.js
@@ -0,0 +1,15 @@
+import addSvg from "../assets/images/add.svg";
+
+export default function addTask() {
+  const button = document.createElement("button");
+  const span = document.createElement("span");
+  const img = document.createElement("img");
+
+  img.src = addSvg;
+  span.append(img);
+
+  button.appendChild(span);
+  button.appendChild(document.createTextNode("Add task"));
+
+  return button;
+}
