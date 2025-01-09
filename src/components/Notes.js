dc1f66ed052bac2bea849818573a831b13290694
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-09 11:38:37 +0800

    Create Notes Class

---
+++ src/components/Notes.js
@@ -0,0 +1,22 @@
+export default class Notes {
+  constructor(title, content = "") {
+    this.title = title;
+    this.content = content;
+  }
+
+  setTitle(title) {
+    this.title = title;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  setContent(content) {
+    this.content = content;
+  }
+
+  getContent() {
+    return this.content;
+  }
+}
