62f5403ce8eb8312f49bfb9c01616160324234a4
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-09 13:09:34 +0800

    Add classes to divs

--- src/components/loadContent.js
+++ src/components/loadContent.js
@@ -6,11 +6,14 @@ export default function loadContent(name = "Inbox") {
   content.innerHTML = "";
 
   const container = document.createElement("div");
+  container.classList.add("container");
 
   const containerTitle = document.createElement("h1");
+  containerTitle.classList.add("container-title");
   containerTitle.textContent = name;
 
   const containerContent = document.createElement("div");
+  containerContent.classList.add("container-content");
 
   const addTaskBTN = addTaskButton();
 
