eb857a1d95f0a1e66349bd0d3a20ff9649cf9463
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-09 11:38:24 +0800

    Create Projects Class

---
+++ src/components/Projects.js
@@ -0,0 +1,35 @@
+import TodoList from "./TodoList";
+
+export default class Project {
+  constructor(title, description = "") {
+    this.project = {
+      title: title,
+      description: description,
+      list: new TodoList(),
+    };
+  }
+
+  setTitle(title) {
+    this.project.title = title;
+  }
+
+  getTitle() {
+    return this.project.title;
+  }
+
+  setDescription(description) {
+    this.project.description = description;
+  }
+
+  getDescription() {
+    return this.project.description;
+  }
+
+  setList(title, description, dueDate, priority) {
+    this.project.list.setList(title, description, dueDate, priority);
+  }
+
+  getList() {
+    return this.project.list.getList();
+  }
+}
