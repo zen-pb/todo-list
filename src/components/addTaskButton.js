0ec30ebac051bc2b6a41e36a4e463c6e2304eb98
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-09 13:02:28 +0800

    Rename to know what it does at single look

--- src/components/addTaskButton.js
+++ src/components/addTaskButton.js
@@ -1,6 +1,6 @@
 import addSvg from "../assets/images/add.svg";
 
-export default function addTask() {
+export default function addTaskButton() {
   const button = document.createElement("button");
   const span = document.createElement("span");
   const img = document.createElement("img");
