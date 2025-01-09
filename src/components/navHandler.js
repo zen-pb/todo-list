77d667508817028a95222d6ae323baaa8d96751f
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-09 12:57:16 +0800

    Fix navHandler

--- src/components/navHandler.js
+++ src/components/navHandler.js
@@ -1,20 +1,27 @@
+import loadContent from "./loadContent";
+
 export default function navHandler() {
   const navButtons = document.querySelectorAll("nav button");
 
   const routes = {
-    inbox,
-    today,
-    week,
-    projects,
-    notes,
+    inbox: "Inbox",
+    today: "Today",
+    week: "This Week",
+    projects: "Projects",
+    notes: "Notes",
   };
 
   navButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-      const route = routes[e.target.id];
+      const target = e.target.closest("button");
+
+      const route = routes[target.id];
+
       if (route) {
-        return route;
+        loadContent(route);
       }
     });
   });
+
+  loadContent();
 }
