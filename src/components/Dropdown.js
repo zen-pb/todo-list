3473a56235da3548ff7d2e206f555c321084c397
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-12 18:03:17 +0800

    Add class to button and add length handler

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,14 +2,22 @@ export default function Dropdown(button, list) {
   const div = document.createElement("div");
   div.classList.add(`${button.id}-dropdown`);
 
+  button.classList.add("dropdown-button");
+
   const ul = document.createElement("ul");
   ul.classList.add("dropdown-items");
 
-  list.forEach((item) => {
+  if (list.length > 1) {
+    list.forEach((item) => {
+      const li = document.createElement("li");
+      li.appendChild(item);
+      ul.appendChild(li);
+    });
+  } else {
     const li = document.createElement("li");
-    li.appendChild(item);
+    li.appendChild(list);
     ul.appendChild(li);
-  });
+  }
 
   div.append(button, ul);
 
