e313ef73c76b1be94745c25c1883bb65f2c8437b
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-16 21:23:28 +0800

    Add structure

--- src/components/generateNoteForm.js
+++ src/components/generateNoteForm.js
@@ -1,1 +1,31 @@
-export default function generateNoteForm() {}
+import Button from "./Button";
+
+export default function generateNoteForm() {
+  const form = document.createElement("form");
+  form.classList.add("note-form");
+
+  const formText = document.createElement("div");
+  formText.classList.add("form-text");
+
+  const titleInput = document.createElement("input");
+  titleInput.type = "text";
+  titleInput.placeholder = "Title";
+  titleInput.required = true;
+
+  const descriptionInput = document.createElement("input");
+  descriptionInput.type = "text";
+  descriptionInput.placeholder = "Take a note...";
+
+  formText.append(titleInput, descriptionInput);
+
+  const actionsBTN = document.createElement("div");
+  actionsBTN.classList.add("actions-btn");
+
+  const closeBTN = Button("Close");
+
+  actionsBTN.append(closeBTN);
+
+  form.append(formText, actionsBTN);
+
+  return form;
+}
