262388fd2a436b964b31f104c44d99ea59f74119
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-18 22:37:41 +0800

    Initialize structure

---
+++ src/components/loadNotes.js
@@ -0,0 +1,35 @@
+import Storage from "../classes/Storage";
+
+export default function loadNotes() {
+  const notes = Storage.getStorage("notes");
+
+  const container = document.createElement("div");
+
+  notes.forEach((note) => {
+    const card = document.createElement("div");
+    card.classList.add("note-card");
+
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("note-card-titleDiv");
+
+    const title = document.createElement("h1");
+    title.classList.add("note-card-title");
+    title.textContent = note.title;
+
+    titleDiv.append(title);
+
+    const contentsDiv = document.createElement("div");
+    contentsDiv.classList.add("note-card-contentsDiv");
+
+    const content = document.createElement("p");
+    content.classList.add("note-card-content");
+    content.textContent = note.content;
+
+    contentsDiv.append(details);
+
+    card.append(titleDiv, contentsDiv);
+    container.appendChild(card);
+  });
+
+  return container;
+}
