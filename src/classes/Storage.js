c0de911d58dd08104598a88ea92acf6a076b0019
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-18 14:51:08 +0800

    Add note handler to the Storage

--- src/classes/Storage.js
+++ src/classes/Storage.js
@@ -1,9 +1,19 @@
 export default class Storage {
-  static setStorage(data) {
-    localStorage.setItem("todo-list", JSON.stringify(data));
+  static setStorage(storageName, data) {
+    let storedItem;
+
+    if (storageName == "notes") {
+      storedItem = this.getStorage(storageName)
+        ? this.getStorage(storageName)
+        : [];
+
+      storedItem.push(data);
+    }
+
+    localStorage.setItem(storageName, JSON.stringify(storedItem));
   }
 
-  static getStorage() {
-    return JSON.parse(localStorage.getItem("todo-list"));
+  static getStorage(storageName) {
+    return JSON.parse(localStorage.getItem(storageName));
   }
 }
