a348e692d9d35425fc6484996fee7e5d1e449de0
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-18 15:01:35 +0800

    Add a projects handler

--- src/classes/Storage.js
+++ src/classes/Storage.js
@@ -1,6 +1,16 @@
+import Project from "./Projects";
+
 export default class Storage {
   static setStorage(storageName, data) {
     let storedItem;
+
+    if (storageName == "projects") {
+      storedItem = this.getStorage(storageName)
+        ? this.getStorage(storageName)
+        : [new Project("Inbox")];
+
+      storedItem.push(data);
+    }
 
     if (storageName == "notes") {
       storedItem = this.getStorage(storageName)
