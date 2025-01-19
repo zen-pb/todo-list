d370a2ffcdbc4560c27a32ccdca26a1e947a0ee5
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-19 21:30:15 +0800

    Add projects svg

--- src/components/loadProjects.js
+++ src/components/loadProjects.js
@@ -1,6 +1,7 @@
 import Storage from "../classes/Storage";
 import Button from "./Button";
 import dropdownSvg from "../assets/images/dropdown.svg";
+import projectSvg from "../assets/images/projects-dropdown.svg";
 
 export default function loadProjects() {
   const projects = Storage.getStorage("projects");
@@ -15,7 +16,7 @@ export default function loadProjects() {
       const projectDiv = document.createElement("div");
       projectDiv.classList.add("project-div");
 
-      const title = Button(key);
+      const title = Button(key, projectSvg);
       const dropdown = document.createElement("img");
       dropdown.src = dropdownSvg;
 
