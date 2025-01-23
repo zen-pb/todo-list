5c3be0689fe45a90764de27948261a85f39ddfb7
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-23 23:26:46 +0800

    Fix structure

--- src/components/loadTodos.js
+++ src/components/loadTodos.js
@@ -6,29 +6,30 @@ export default function loadTodos() {
 
   const container = document.createElement("div");
   container.classList.add("todo-container");
+
   todos.forEach((todo) => {
-    const checkBTN = document.createElement("button");
-    checkBTN.classList.add("check");
-
-    const img = document.createElement("img");
-    img.src = checkSvg;
-
-    checkBTN.append(img);
-
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo-div");
-
-    const todoTextDiv = document.createElement("div");
-    todoTextDiv.classList.add("todo-text");
-
-    const todoOtherObj = document.createElement("div");
-    todoOtherObj.classList.add("todo-others");
-
     const [key] = Object.keys(todo);
 
     const todoItem = todo[key];
 
     todoItem.list.forEach((todoInfo) => {
+      const todoDiv = document.createElement("div");
+      todoDiv.classList.add("todo-div");
+
+      const todoTextDiv = document.createElement("div");
+      todoTextDiv.classList.add("todo-text");
+
+      const todoOtherObj = document.createElement("div");
+      todoOtherObj.classList.add("todo-others");
+
+      const checkBTN = document.createElement("button");
+      checkBTN.classList.add("check");
+
+      const img = document.createElement("img");
+      img.src = checkSvg;
+
+      checkBTN.append(img);
+
       const title = document.createElement("p");
       title.textContent = todoInfo.title;
 
