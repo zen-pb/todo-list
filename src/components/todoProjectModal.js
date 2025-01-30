55a24cbb51b4cf172dc331a18e0ba4034d73df41
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-30 22:02:19 +0800

    Add todoProjectModal

---
+++ src/components/todoProjectModal.js
@@ -0,0 +1,15 @@
+export default function todoProjectModal(projectDiv) {
+  const div = document.createElement("div");
+
+  const containerTitle = document.createElement("h1");
+  containerTitle.classList.add("container-title");
+  containerTitle.textContent =
+    projectDiv.querySelector(".project-name").textContent;
+
+  const containerContent = document.createElement("div");
+  containerContent.classList.add("container-content");
+
+  div.append(containerTitle, containerContent);
+
+  return div;
+}
